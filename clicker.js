8a06273c2f5f107de036c548d5698ba9f5113486
Benjamin Ißler <b.issler@mwaysolutions.com>
2017-06-04 18:37:51 +0200

    start practicing immediately and at the end jump to start

--- clicker.js
+++ clicker.js
@@ -1,7 +1,27 @@
+// Start practicing immediately
+document.arrive('.btn.btn-white.btn-outline.practice', function () {
+  console.info('Practice Button is in DOM');
+  document.querySelector(".btn.btn-white.btn-outline.practice").click();
+  console.info('Practice Button clicked');
+});
+
+// Start regular lesson immediately with green button
+document.arrive('.btn.btn-green.btn-block.skill-item', function () {
+  console.info('Green Lesson Button is in DOM');
+  document.querySelector('.btn.btn-green.btn-block.skill-item').click();
+  console.info('Green Lesson Button clicked');
+});
+
+// Start strengthen lesson immediately
 document.arrive('#untimed-button', function () {
-  console.info('Button is in DOM');
+  console.info('Start Button is in DOM');
   document.querySelector('#untimed-button').click();
-  console.info('Button clicked');
-  //document.unbindArrive('#untimed-button');
-  //console.info('Button unbinded');
+  console.info('Start Button clicked');
+});
+
+// On end of lesson, jump to start
+document.arrive('.continue.btn.btn-lg.btn-green.right', function () {
+  console.info('Next Button is in DOM');
+  document.querySelector("#home-nav a").click();
+  console.info('Home Button clicked');
 });
