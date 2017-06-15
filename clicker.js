624c671370780fc633e34d3b19b2b712aaa3c77c
Benjamin Ißler <b.issler@mwaysolutions.com>
2017-06-15 18:29:13 +0200

    update to new duolingo layout

--- clicker.js
+++ clicker.js
@@ -1,27 +1,50 @@
-// Start practicing immediately
-document.arrive('.btn.btn-white.btn-outline.practice', function () {
-  console.info('Practice Button is in DOM');
-  document.querySelector(".btn.btn-white.btn-outline.practice").click();
-  console.info('Practice Button clicked');
-});
+function clickButton (button, targetbutton) {
+  document.arrive(button.query, function () {
+    console.info(button.name + ' is in DOM');
+    if (!targetbutton) {
+      document.querySelector(button.query).click();
+      console.info(button.name + ' clicked');
+    } else {
+      document.querySelector(targetbutton.query).click();
+      console.info(targetbutton.name + ' clicked');
+    }
+  });  
+}
 
-// Start regular lesson immediately with green button
-document.arrive('.btn.btn-green.btn-block.skill-item', function () {
-  console.info('Green Lesson Button is in DOM');
-  document.querySelector('.btn.btn-green.btn-block.skill-item').click();
-  console.info('Green Lesson Button clicked');
-});
 
-// Start strengthen lesson immediately
-document.arrive('#untimed-button', function () {
-  console.info('Start Button is in DOM');
-  document.querySelector('#untimed-button').click();
-  console.info('Start Button clicked');
-});
+// Lesson where you haven't learned all sessions yet
+var anfangen = {
+  'query': 'a[data-test="begin-session-button"]',
+  'name': 'Anfangen Button'
+}
+clickButton(anfangen);
 
-// On end of lesson, jump to start
-document.arrive('.continue.btn.btn-lg.btn-green.right', function () {
-  console.info('Next Button is in DOM');
-  document.querySelector("#home-nav a").click();
-  console.info('Home Button clicked');
-});
+
+// Lesson where you have learned all sessions and are 
+// repeating lessons to strengthen words. This button only 
+// gets clicked if you have learned all lessons so far
+var staerken = {
+  'query': 'a[data-test="skill-header-practice-button"][href$=practice]',
+  'name': 'Stärken Button'
+}
+clickButton(staerken);
+
+
+// Skip the Duolingo Owl on beginning of a sessions
+var uebunganfangen = {
+  'query': 'button[data-test="player-next"]',
+  'name': 'Übung anfangen Button'
+}
+clickButton(uebunganfangen);
+
+
+// When lesson ends, immediately go to start
+var end = {
+  'query': 'div[data-test="player-end-carousel"]',
+  'name': 'Lektion abgeschlossen',
+}
+var home = {
+  'query': 'a[data-test="home-nav"]',
+   'name': 'Startseite'
+}
+clickButton(end, home);
